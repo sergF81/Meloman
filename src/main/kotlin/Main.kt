d8ed3eb5304f2a95c9ced70d4e36484d598db0e9
Sergey Fortis <s_fortis@mail.ru>
2021-05-12 20:50:34 +0500

    Доработка Меломан

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,43 +1,31 @@
 fun main() {
 
-    var meloman: Boolean = false
-
     while (true) {
         println("________________________________________")
         println("Enter the amount of new Purchase.")
         var newPurchase = readLine()?.toDouble();
 
         println("Are you a regular customer? y/n")
-        val melomanInput = readLine();
-        if (melomanInput == "y") {
-            meloman = true
-        }
+        var melomanInput = readLine();
 
         println("Enter the amount of previous Purchase.")
         val previousPurchase = Integer.parseInt(readLine());
 
-        if (1000 < previousPurchase && previousPurchase <= 10000) {
-            println("Discount is 100 rubles!")
-            if (newPurchase != null) {
-                newPurchase = newPurchase - 100
+        newPurchase = when {
+            1000 < previousPurchase && previousPurchase <= 10000 -> {
+                println("Discount is 100 rubles!")
+                newPurchase?.minus(100)
             }
-
-        }
-        if (10001 < previousPurchase) {
-            println("Discount is 5%  of amount Purchase!")
-            if (newPurchase != null) {
-                newPurchase = newPurchase - (newPurchase / 100) * 5
+            10001 < previousPurchase -> {
+                println("Discount is 5%  of amount Purchase!")
+                newPurchase?.minus((newPurchase / 100) * 5)
             }
-
+            else -> newPurchase
         }
-        if (meloman == true) {
+        if (melomanInput == "y") {
             println("Discount is 1%  of amount Purchase!")
-            if (newPurchase != null) {
-                newPurchase = newPurchase - newPurchase / 100
-            }
-
+            newPurchase = newPurchase?.minus(newPurchase / 100)
         }
-
         println("Result amount is $newPurchase")
     }
 }
